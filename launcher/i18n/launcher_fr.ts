01aa9b602874063bb9b541ced85cc51dc981a742
Fabien Caylus <tranqyll.dev@gmail.com>
2016-05-05 10:08:18 +0200

    Update translation

--- launcher/i18n/launcher_fr.ts
+++ launcher/i18n/launcher_fr.ts
@@ -4,7 +4,7 @@
 <context>
     <name>ActivityMusic</name>
     <message>
-        <location filename="../qml/ActivityMusic.qml" line="200"/>
+        <location filename="../qml/ActivityMusic.qml" line="194"/>
         <source>New playlist&apos;s name:</source>
         <translation>Nom de la nouvelle playlist:</translation>
     </message>
@@ -12,32 +12,32 @@
 <context>
     <name>ActivitySettings</name>
     <message>
-        <location filename="../qml/ActivitySettings.qml" line="55"/>
+        <location filename="../qml/ActivitySettings.qml" line="46"/>
         <source>Equalizer</source>
         <translation>Égaliseur</translation>
     </message>
     <message>
-        <location filename="../qml/ActivitySettings.qml" line="61"/>
+        <location filename="../qml/ActivitySettings.qml" line="52"/>
         <source>Remove files</source>
         <translation>Supprimer des fichiers</translation>
     </message>
     <message>
-        <location filename="../qml/ActivitySettings.qml" line="67"/>
+        <location filename="../qml/ActivitySettings.qml" line="58"/>
         <source>Update</source>
         <translation>Mise à jour</translation>
     </message>
     <message>
-        <location filename="../qml/ActivitySettings.qml" line="73"/>
+        <location filename="../qml/ActivitySettings.qml" line="64"/>
         <source>Languages</source>
         <translation>Langues</translation>
     </message>
     <message>
-        <location filename="../qml/ActivitySettings.qml" line="79"/>
+        <location filename="../qml/ActivitySettings.qml" line="70"/>
         <source>System</source>
         <translation>Système</translation>
     </message>
     <message>
-        <location filename="../qml/ActivitySettings.qml" line="85"/>
+        <location filename="../qml/ActivitySettings.qml" line="76"/>
         <source>About</source>
         <translation>À propos</translation>
     </message>
@@ -50,32 +50,32 @@
         <translation>Rafraichir</translation>
     </message>
     <message>
-        <location filename="../qml/ActivityTransfer.qml" line="65"/>
+        <location filename="../qml/ActivityTransfer.qml" line="63"/>
         <source>USB sticks list :</source>
         <translation>Liste des clés USB :</translation>
     </message>
     <message>
-        <location filename="../qml/ActivityTransfer.qml" line="77"/>
+        <location filename="../qml/ActivityTransfer.qml" line="75"/>
         <source>Please insert a USB stick !</source>
         <translation>Veuillez insérer une clé USB !</translation>
     </message>
     <message>
-        <location filename="../qml/ActivityTransfer.qml" line="156"/>
+        <location filename="../qml/ActivityTransfer.qml" line="154"/>
         <source>Are you sure to transfer all music files from this device ?</source>
         <translation>Êtes-vous sûr de vouloir transférer tous les fichiers musicaux depuis ce périphérique ?</translation>
     </message>
     <message>
-        <location filename="../qml/ActivityTransfer.qml" line="166"/>
+        <location filename="../qml/ActivityTransfer.qml" line="164"/>
         <source>Copy all files from device ...</source>
         <translation>Copie de tous les fichiers du périphérique ...</translation>
     </message>
     <message>
-        <location filename="../qml/ActivityTransfer.qml" line="197"/>
+        <location filename="../qml/ActivityTransfer.qml" line="195"/>
         <source>Are you sure to delete all media stored in the radio ?</source>
         <translation>Êtes-vous sûr de vouloir supprimer tous les médias stockés localement ?</translation>
     </message>
     <message>
-        <location filename="../qml/ActivityTransfer.qml" line="210"/>
+        <location filename="../qml/ActivityTransfer.qml" line="208"/>
         <source>Deleting local media files ...</source>
         <translation>Suppression des médias locaux ...</translation>
     </message>
@@ -128,6 +128,49 @@
         <location filename="../qml/music/ListViewPlaylist.qml" line="127"/>
         <source>New playlist</source>
         <translation>Nouvelle playlist</translation>
+    </message>
+</context>
+<context>
+    <name>MainStackView</name>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="83"/>
+        <source>My Music</source>
+        <translation>Ma Musique</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="89"/>
+        <source>Radio</source>
+        <translation>Radio</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="95"/>
+        <source>My Videos</source>
+        <translation>Mes Vidéos</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="101"/>
+        <source>Informations</source>
+        <translation>Informations</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="107"/>
+        <source>GPS</source>
+        <translation>GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="113"/>
+        <source>Call</source>
+        <translation>Appeller</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="119"/>
+        <source>Transfer</source>
+        <translation>Transférer</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainStackView.qml" line="125"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
     </message>
 </context>
 <context>
@@ -191,12 +234,12 @@
         <translation>Playlists ...</translation>
     </message>
     <message>
-        <location filename="../qml/music/MusicListView.qml" line="279"/>
+        <location filename="../qml/music/MusicListView.qml" line="282"/>
         <source>Are you sure to remove &quot;%1&quot; playlist ?</source>
         <translation>Voulez-vous vraiment supprimer la playlist &quot;%1&quot; ?</translation>
     </message>
     <message>
-        <location filename="../qml/music/MusicListView.qml" line="296"/>
+        <location filename="../qml/music/MusicListView.qml" line="299"/>
         <source>Are you sure to remove &quot;%1&quot; from the playlist ?</source>
         <translation>Voulez-vous vraiment supprimer &quot;%1&quot; de la playlist ?</translation>
     </message>
@@ -212,7 +255,7 @@
 <context>
     <name>PasswordPrompt</name>
     <message>
-        <location filename="../qml/PasswordPrompt.qml" line="98"/>
+        <location filename="../qml/PasswordPrompt.qml" line="103"/>
         <source>Show password</source>
         <translation>Afficher le mot de passe</translation>
     </message>
@@ -271,27 +314,27 @@
         <translation>Licenses Tièrces</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsAbout.qml" line="116"/>
+        <location filename="../qml/settings/SettingsAbout.qml" line="110"/>
         <source>Full featured headunit system !</source>
         <translation>Système d&apos;autoradio complet !</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsAbout.qml" line="126"/>
+        <location filename="../qml/settings/SettingsAbout.qml" line="120"/>
         <source>Version:</source>
         <translation>Version:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsAbout.qml" line="127"/>
+        <location filename="../qml/settings/SettingsAbout.qml" line="121"/>
         <source>Hardware version:</source>
         <translation>Version matèrielle:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsAbout.qml" line="137"/>
+        <location filename="../qml/settings/SettingsAbout.qml" line="131"/>
         <source>LibVLC version:</source>
         <translation>Version de LibVLC:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsAbout.qml" line="138"/>
+        <location filename="../qml/settings/SettingsAbout.qml" line="132"/>
         <source>VLC-Qt version:</source>
         <translation>Version de VLC-Qt:</translation>
     </message>
@@ -299,7 +342,7 @@
 <context>
     <name>SettingsLanguage</name>
     <message>
-        <location filename="../qml/settings/SettingsLanguage.qml" line="53"/>
+        <location filename="../qml/settings/SettingsLanguage.qml" line="51"/>
         <source>Languages list:</source>
         <translation>Langues disponibles:</translation>
     </message>
@@ -307,37 +350,37 @@
 <context>
     <name>SettingsSystem</name>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="43"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="41"/>
         <source>Kernel:</source>
         <translation>Noyau:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="50"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="48"/>
         <source>Kernel version:</source>
         <translation>Version du noyau:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="57"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="55"/>
         <source>CPU architecture:</source>
         <translation>Architecture du processeur:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="64"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="62"/>
         <source>Word size:</source>
         <translation>Taille des mots:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="71"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="69"/>
         <source>Build ABI:</source>
         <translation>ABI:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="78"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="76"/>
         <source>Build date:</source>
         <translation>Date de compilation:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsSystem.qml" line="136"/>
+        <location filename="../qml/settings/SettingsSystem.qml" line="134"/>
         <source>Free space:</source>
         <translation>Espace libre:</translation>
     </message>
@@ -345,57 +388,57 @@
 <context>
     <name>SettingsUpdate</name>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="32"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="30"/>
         <source>Available update packages :</source>
         <translation>Paquets de mise à jour disponibles:</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="44"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="42"/>
         <source>Searching ...</source>
         <translation>Recherche en cours ...</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="59"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="57"/>
         <source>No updates found !</source>
         <translation>Aucune mise à jour trouvée !</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="104"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="102"/>
         <source>Update to version: %1</source>
         <translation>Mise à jour vers la version %1</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="105"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="103"/>
         <source>Older package (version %1)</source>
         <translation>Vieux paquet (version %1)</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="106"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="104"/>
         <source>Different hardware package (manual update): %1</source>
         <translation>Paquet de màj. système (procédure manuelle): %1</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="107"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="105"/>
         <source>Malformed package !</source>
         <translation>Paquet de màj mal-formé !</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="174"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="172"/>
         <source>Are you sure to update your system to version %1 ?</source>
         <translation>Êtes-vous sûr de vouloir faire la mise à jour vers la version %1 ?</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="189"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="187"/>
         <source>The update can turn your system into a brick. Are you sure ?</source>
         <translation>La mise à jour peut rendre votre système inutilisable. Êtes-vous sûr ?</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="204"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="202"/>
         <source>Really ?</source>
         <translation>Vraiment ?</translation>
     </message>
     <message>
-        <location filename="../qml/settings/SettingsUpdate.qml" line="217"/>
+        <location filename="../qml/settings/SettingsUpdate.qml" line="215"/>
         <source>Enter root password to continue:</source>
         <translation>Entrer le mot de passe admin pour continuer:</translation>
     </message>
@@ -403,53 +446,13 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="90"/>
-        <source>My Music</source>
-        <translation>Ma Musique</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="96"/>
-        <source>Radio</source>
-        <translation>Radio</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="102"/>
-        <source>My Videos</source>
-        <translation>Mes Vidéos</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="108"/>
-        <source>Informations</source>
-        <translation>Informations</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="114"/>
-        <source>GPS</source>
-        <translation>GPS</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="120"/>
-        <source>Call</source>
-        <translation>Appeller</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="126"/>
-        <source>Transfer</source>
-        <translation>Transférer</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="132"/>
-        <source>Settings</source>
-        <translation>Paramètres</translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="159"/>
+        <location filename="../qml/main.qml" line="70"/>
         <source>It&apos;s your first radio boot !
 Please enter a new password :</source>
         <translation>C&apos;est la première fois que vous allumez votre autoradio !\nVeuillez entrez un nouveau mot de passe :</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="173"/>
+        <location filename="../qml/main.qml" line="87"/>
         <source>Please confirm :</source>
         <translation>Veuillez confirmer:</translation>
     </message>
